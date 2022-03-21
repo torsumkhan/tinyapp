cfe71f11f2f0284967c4e6a15e4ad32a2cf33e79
torsumkhan <torsum.khan@gmail.com>
2022-03-22 00:54:22 +0500

    added 2 more routes

--- express_server.js
+++ express_server.js
@@ -13,9 +13,19 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get('/urls.json', (req, res) => {
-    res.json(urlDatabase);
+app.get('/urls/:shortURL', (req, res) => {
+    const templateVars = {shortURL: req.params.shortURL, longURL:req.params.longURL};
+    res.render('urls_show', templateVars);
 })
+
+app.get('/urls', (req, res) => {
+    const templateVars = {urls: urlDatabase};
+    res.render('urls_index', templateVars);
+})
+
+// app.get('/urls.json', (req, res) => {
+//     res.json(urlDatabase);
+// })
 
 app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>World</b></body></html>\n')
