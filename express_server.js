b22fdf1cefb69d018975ee9ac3ee97d2eaa97787
torsumkhan <torsum.khan@gmail.com>
2022-03-22 01:22:10 +0500

    added 2 pages

--- express_server.js
+++ express_server.js
@@ -23,9 +23,9 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 })
 
-// app.get('/urls.json', (req, res) => {
-//     res.json(urlDatabase);
-// })
+app.get('/urls.json', (req, res) => {
+    res.json(urlDatabase);
+})
 
 app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>World</b></body></html>\n')
