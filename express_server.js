3463ff17e5fa9d552d9a749aae521cb59f7e42ab
torsumkhan <torsum.khan@gmail.com>
2022-03-23 04:35:18 +0500

    completed cookies

--- express_server.js
+++ express_server.js
@@ -3,8 +3,10 @@ const req = require("express/lib/request");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const res = require("express/lib/response");
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -22,16 +24,18 @@ app.get("/", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-    const templateVars = {urls: urlDatabase};
+    const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
     res.render('urls_index', templateVars);
 })
 
 app.get('/urls/new', (req,res) => {
-    res.render('urls_new');
+    const templateVars = {username: req.cookies["username"]}
+    res.render('urls_new', templateVars);
 })
 
 app.get('urls/:id', (req, res) => {
-    res.render('urls_id')
+    const templateVars = {username: req.cookies["username"]}
+    res.render('urls_id', templateVars)
 })
 
 app.post('/urls/:shortURL', (req,res) => {
@@ -41,7 +45,7 @@ app.post('/urls/:shortURL', (req,res) => {
 })
 
 app.get('/urls/:shortURL', (req, res) => {
-    const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+    const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
     console.log(req.params)
     res.render('urls_show', templateVars);
 })
@@ -73,6 +77,18 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>World</b></body></html>\n')
+})
+
+app.post('/login', (req,res) => {
+    res.cookie('username', req.body.username)
+    res.redirect('/urls')
+
+})
+
+app.post('/logout', (req,res) => {
+    res.clearCookie('username')
+    res.redirect('/urls')
+
 })
 
 
